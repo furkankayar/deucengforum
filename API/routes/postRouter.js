deafa63b1372217c8ef31825d70965d64463ed31
Furkan Kayar <furkankayar427@hotmail.com>
2020-01-24 00:50:49 +0300

    Test

--- API/routes/postRouter.js
+++ API/routes/postRouter.js
@@ -6,6 +6,5 @@ module.exports = (expressRouter, expressApp, postHelper) => {
   expressRouter.put('/new_post', expressApp.oauth.authorise(), postHelper.createNewPost);
   expressRouter.post('/get_posts', postHelper.getPosts);
 
-
   return expressRouter;
 }
